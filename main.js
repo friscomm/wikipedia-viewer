d8dab67abcafad9be4e414594c8c77d87dcc3a7e
Frisco Miranda <friscomiranda@gmail.com>
2017-01-25 12:51:25 -0800

    remove random button and fix filter to refresh with each new search

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
 
+  let searched;
+
   //attach search function to the button
   $(".search-btn").click(function(){
     //this pulls the text from the input field so you can use it to search wikipedia
-    console.log("jquery working");
     var searchTerm = $("input").val();
 
     //this gets the JSON data you need and incorporates the search term you pulled above. it must end with that callback and needs to be in json format
@@ -14,14 +15,21 @@ $(document).ready(function() {
       };
 
    //iterate through the data to get it into the correct format. the data is all there, it's just formatted a bit funky. the JSON call above will group all the titles, descriptions, and urls with each other. but we want to have each group consist of the related title, description, and url. the code below does this by iterating through each group and pulling the items at the same spot in the three different arrays
+
       for (i = 0; i < data[1].length; i++) {
         var title = data[1][i];
         var description = data[2][i];
         var link = data[3][i];
+        let emptySpace = $(".newStuff").html();
         //combine the data with some html in order to get boxes to appear that are clickable
-        var combinedData = "<a href='" + link + "'>" + "<div class = 'well search_result'> " + "<div class='name'>" + title + "</div>" + description + "</div></a>";
+        var combinedData = "<a href='" + link + "'>" + "<div class = 'well search_result'> " + "<div class='name'>" + title.toUpperCase() + "</div>" + description + "</div></a>";
+
+        if(i==0 && emptySpace !== ''){
+          $(".newStuff").html('');
+        }
         //put all that data on the page
         $(".newStuff").append(combinedData);
+        searched = true;
       }
     });
   });
